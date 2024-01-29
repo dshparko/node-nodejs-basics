117e46a9763dabb52f4fdad50f1f56511b673858
dshparko <d.shparko@mail.ru>
2024-01-29 20:11:21 +0300

    feat: add solution (zip) x3

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import {createReadStream, createWriteStream} from "fs";
+import {createGzip} from "zlib";
+import {pipeline} from "stream";
+
+const gzip = createGzip();
+const sourceFile = createReadStream('./src/zip/files/fileToCompress.txt');
+const destinationFile = createWriteStream('./src/zip/files/archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    pipeline(sourceFile, gzip, destinationFile, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        }
+      });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import {createReadStream, createWriteStream} from "fs";
+import zlib from "zlib";
+
+const sourceFile = createWriteStream('./src/zip/files/fileToCompress.txt');
+const destinationFile = createReadStream('./src/zip/files/archive.gz');
+const unzip = zlib.createGunzip();
+    
 const decompress = async () => {
-    // Write your code here 
+   
+    destinationFile.pipe(unzip).pipe(sourceFile);
 };
 
 await decompress();
